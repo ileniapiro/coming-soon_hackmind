80ab15d0823c042139812de8c2cad007c0b011e8
Ilenia Piro <ilenia.piro@gmail.com>
2020-03-01 16:50:11 +0100

    First commit.

---
+++ js/app.js
@@ -0,0 +1,37 @@
+/*
+* A Design by Ilenia Piro
+* Author: Ilenia Piro
+* Author URL:
+* License: Copyright © 2020 Hackmind di Proietti Claudio
+ */
+$(function(){
+	'use strict';
+
+	var options ={
+		wrapper: ".wrapper",
+		minHeight: 500
+	};
+
+	function setHeight(){
+		var documentHeight = $(window).height();
+
+		if(documentHeight > options.minHeight){
+			$(options.wrapper).height(documentHeight);
+		}
+	}
+
+	setHeight();
+
+	$(window).resize(function(){
+		setHeight();
+	})
+
+	$('.countdown').downCount({
+                    date: '01/05/2020 9:00:00',
+                    offset: +10
+                }, function() {
+
+                });
+
+
+});
