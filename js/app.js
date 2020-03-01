b8bc405d0b02610a98b4d23eb27f4378701bd4f2
Ilenia Piro <ilenia.piro@gmail.com>
2020-03-01 19:02:05 +0100

    Pre-publish commit

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
 /*
-* A Design by Ilenia Piro
-* Author: Ilenia Piro
-* Author URL:
-* License: Copyright © 2020 Hackmind di Proietti Claudio
+ * A Design by Ilenia Piro
+ * Author: Ilenia Piro
+ * Author URL:
+ * License: Copyright © 2020 Hackmind di Proietti Claudio
  */
-$(function(){
-	'use strict';
+$(function() {
+  'use strict';
 
-	var options ={
-		wrapper: ".wrapper",
-		minHeight: 500
-	};
+  var options = {
+    wrapper: ".wrapper",
+    minHeight: 500
+  };
 
-	function setHeight(){
-		var documentHeight = $(window).height();
+  function setHeight() {
+    var documentHeight = $(window).height();
 
-		if(documentHeight > options.minHeight){
-			$(options.wrapper).height(documentHeight);
-		}
-	}
+    if (documentHeight > options.minHeight) {
+      $(options.wrapper).height(documentHeight);
+    }
+  }
 
-	setHeight();
+  setHeight();
 
-	$(window).resize(function(){
-		setHeight();
-	})
+  $(window).resize(function() {
+    setHeight();
+  })
 
-	$('.countdown').downCount({
-                    date: '01/05/2020 9:00:00',
-                    offset: +10
-                }, function() {
+  $('.countdown').downCount({
+    date: '05/01/2020 9:00:00',
+    offset: +10
+  }, function() {
 
-                });
+  });
 
 
 });
